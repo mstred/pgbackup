66e50a1fb0b0a1e58dd572a5d5bbc6d6cf7c6845
Edson Samuel Jr <samuedson@gmail.com>
2021-08-22 17:29:01 -0300

    Implemented S3 interactions

--- src/pgbackup/storage.py
+++ src/pgbackup/storage.py
@@ -2,3 +2,8 @@ def local(infile, outfile):
     outfile.write(infile.read())
     outfile.close()
     infile.close()
+
+# client = boto3.client('s3')
+def s3(client, infile, bucket, name):
+    client.upload_fileobj(infile, bucket, name)
+
